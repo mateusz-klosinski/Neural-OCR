e1da5710d3ed4c756466252fa2b482ae60e9685f
Mateusz Klosinski <matix003@gmail.com>
2017-05-01 21:21:22 +0200

    Initial Commit

---
+++ Neural-OCR/Network/Neuron.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neural_OCR.Network
+{
+    public class Neuron
+    {
+        
+    }
+}
